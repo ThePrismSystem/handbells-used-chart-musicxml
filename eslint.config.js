91bb3baa0689f624198285e7a1396f073d42a8b6
ThePrismSystem <10910959+ThePrismSystem@users.noreply.github.com>
2026-08-25 17:00:57 -0500

    chore: exclude coverage output from linting

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default tseslint.config(
       "**/node_modules/**",
       "**/vite.config.ts",
       "**/test-setup.ts",
+      "**/coverage/**",
     ],
   },
   ...tseslint.configs.strictTypeChecked,
